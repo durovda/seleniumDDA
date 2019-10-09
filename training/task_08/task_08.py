bb696ebaa63a500fa23c7dfe81751a285b795d18
durovda <55209451+durovda@users.noreply.github.com>
2019-10-09 13:33:05 +0300

    Задание № 08

---
+++ training/task_08/task_08.py
@@ -0,0 +1,23 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://localhost/litecart/en/")
+        self.wait = WebDriverWait(self.driver, 5)  # seconds
+
+    def test_check_stickers(self):
+        products = self.driver.find_elements_by_xpath("//*[contains(@class, 'product column')]")
+        for item in products:
+            stickers = item.find_elements_by_xpath(".//*[contains(@class, 'sticker')]")
+            self.assertEqual(1, len(stickers))
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
